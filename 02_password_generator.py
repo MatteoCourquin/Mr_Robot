3451c9045b06043f426519c0d05bfa049c390355
Matteo <matteo.courquin@gmail.com>
2021-10-01 19:40:00 +0200

    02_password_generator Done

---
+++ 02_password_generator.py
@@ -0,0 +1,29 @@
+
+import random
+import string
+
+
+
+
+length_password = input('How long for your password? : ')
+
+password = []
+
+counter = 0
+
+if length_password.isnumeric():
+
+    while len(password) < int(length_password):
+        counter += 1
+        random_number = (random.randint(1, 9))
+        random_letter = string.ascii_letters
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+        password.append(random_number)
+        password.append(random.choice(random_letter))
+
+    random.shuffle(password)
+    print(f"Your password suggest is : {''.join(map(str, password))}")
+
+else:
+    print("Error ! Please enter a number.")
